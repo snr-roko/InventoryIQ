e013f2be8b7b4977febaaab60f145d74e2891116
mr-roko <rbbagyei@gmail.com>
2024-10-12 02:28:06 +0000

    Warehousestock and storestock models updated to change both's supplier field to manytomany instead of foreign key. This is to prevent complexities with signals of trying to incorporate a uniqueness combination between stock code, supplier and store or warehouse.

--- products/models.py
+++ products/models.py
@@ -22,34 +22,34 @@ class WarehouseStock(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
     reorder_level = models.PositiveIntegerField()
     warehouse = models.ForeignKey(Warehouse, on_delete=models.PROTECT, related_name='warehouse_stocks', null=True, blank=True)
-    supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT, related_name='warehouse_stocks', null=True, blank=True)
+    supplier = models.ManyToManyField(Supplier, related_name='warehouse_stocks', blank=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='warehouse_stocks_created')
 
     class Meta:
-        unique_together = ("stock_code", "warehouse", "supplier")
+        unique_together = ("stock_code", "warehouse")
     
     def __str__(self):
-        return "{} at {} from {}".format(self.product_code, self.warehouse, self.supplier)
+        return "{} at {}".format(self.product_code, self.warehouse)
 
 class StoreStock(models.Model):
     name = models.CharField(max_length=255)
     stock_code = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     quantity = models.PositiveIntegerField(default=0)
-    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT, related_name='store_stocks')
+    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT, related_name='store_stocks') 
     active = models.BooleanField(default=True)
     date_added = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     reorder_level = models.PositiveIntegerField()
     store = models.ForeignKey(Store, on_delete=models.PROTECT, related_name='store_stocks', null=True, blank=True)
-    supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT, related_name='store_stocks', null=True, blank=True)
+    supplier = models.ManyToManyField(Supplier, related_name='store_stocks', blank=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='store_stocks_created')
 
     class Meta:
-        unique_together = ("stock_code", "store", "supplier")
+        unique_together = ("stock_code", "store")
 
     def __str__(self):
-        return "{} at {} from {}".format(self.product_code, self.store, self.supplier)
+        return "{} at {}".format(self.product_code, self.store)
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
