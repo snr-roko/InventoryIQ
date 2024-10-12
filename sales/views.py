9e33a23e2ab74b825aad8452682cc620d3fd9f47
mr-roko <rbbagyei@gmail.com>
2024-10-12 04:04:09 +0000

    Permissions added to various views and signal imported by app when started.

--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import OrderItem, Order, Customer, StockTransfer
 from .serializers import OrderItemSerializer, OrderSerializer, CustomerSerializer, StockTransferSerializer
+from .permissions import CustomerPermissions, OrderItemPermissions, OrderPermissions, StockTransferPermissions
 
 class BaseViewSet(ModelViewSet):
     def perform_create(self, serializer):
@@ -9,16 +10,20 @@ class BaseViewSet(ModelViewSet):
 class CustomerViewSet(BaseViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = (CustomerPermissions,)
 
 class OrderItemViewSet(BaseViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
+    permission_classes = (OrderItemPermissions,)
 
 class OrderViewSet(BaseViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = (OrderPermissions,)
 
 class StockTransferViewSet(BaseViewSet):
     queryset = StockTransfer.objects.all()
     serializer_class = StockTransferSerializer
+    permission_classes = (StockTransferPermissions,)
 
