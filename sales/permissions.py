84b892c8f83a186d65291764002ecf826d00280b
mr-roko <rbbagyei@gmail.com>
2024-10-12 04:00:21 +0000

    Permissions and signals created to restrict access to some actions on the sales view and automatically popular certain models.

--- sales/permissions.py
+++ sales/permissions.py
@@ -27,14 +27,41 @@ class OrderItemPermissions(permissions.BasePermission):
 class OrderPermissions(permissions.BasePermission):
     """
     Admins, Managers, Store Managers and Store Staffs all have full permissions on CRUD operations of the order model
+    A create action can only be done when the data's status is pending
+    An update of status to shipped can only be done when object's status is paid 
+    An update of status to delivered can also only be done when object's status is shipped
+    An update of status to cancelled can also only be done when object's status is paid, shipped or delivered.
+    A destroy action can also only be done when an object's status is pending or cancelled.
     """
+    
+    allowed_roles = ['ADMIN', 'MANAGER', 'STORE_STAFF', 'STORE_MANAGER']
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return request.user.role in ['ADMIN', 'STORE_MANAGER', 'STORE_STAFF', 'MANAGER']
+        if request.user.is_authenticated and request.user.role in self.allowed_roles:
+            if request.method in permissions.SAFE_METHODS:
+                return True
+            elif view.action == 'create':
+                return request.data['status'] == 'PENDING'
+            else:
+                return True 
         else:
             return False        
     def has_object_permission(self, request, view, obj):
-        return self.has_permission(request, view)
+        if request.user.is_authenticated and request.user.role in self.allowed_roles:
+            if request.method in permissions.SAFE_METHODS:
+                return True
+            elif view.action in ['partial_update', 'update']:
+                if request.data['status'] == 'SHIPPED':
+                    return obj.status == 'PENDING'
+                elif request.data['status'] == 'DELIVERED':
+                    return obj.status == 'SHIPPED'
+                elif request.data['status'] == 'CANCELLED':
+                    return obj.status in ['PAID', 'SHIPPED', 'DELIVERED']
+                else:
+                    return request.data['status'] == 'PAID' and obj.status == 'PENDING'
+            elif view.action == 'destroy':
+                return obj.status in ['PENDING', 'CANCELLED']
+        else:
+            return False
     
 class StockTransferPermissions(permissions.BasePermission):
     """
@@ -42,48 +69,53 @@ class StockTransferPermissions(permissions.BasePermission):
         Admins, Managers, Warehouse Managers, Store Managers
 
     Create Permissions
-        A stock Transfer can only be made when status is pending
+        A stock Transfer can only be made when incoming data's status is pending
         Only Admins, Managers, Warehouse Managers can create Stock transfers
 
     Update Permissions
-        Admins and Managers have full permission
-        Warehouse Managers can only make updates when Status is pending
-        Store Managers can only make updates when update is received but can not update quantity though
-
+        If status is not being changed and the object's status is pending then user must be a warehouse manager, manager or admin
+        If status is being updated to received and quantity is not being updated then user must be a store manager, manager or admin
         Cancelled permissions
-            Only Managers and Admins can update status to cancelled
+            A status can be updated to cancelled when object's status is received.
+            User must be a warehouse manager, store manager, manager or admin
             Cancelled will revert stocks back to warehouse
-
+        other update permissions are not allowed
     Delete Permissions
-        Only when status is pending
-        Only Warehouse Managers, Managers and Admins have access
+        An object can be deleted when object's status is pending or cancelled.
+        If pending, then user must be a warehouse manager, manager or admin
+        If cancelled, then user must be a store manager, manager or admin
     """
-
+    allowed_roles = ['ADMIN', 'MANAGER', 'WAREHOUSE_MANAGER', 'STORE_MANAGER']
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             if request.method in permissions.SAFE_METHODS:
-                return request.user.role in ['ADMIN', 'MANAGER', 'WAREHOUSE_MANAGER', 'STORE_MANAGER']
+                return request.user.role in self.allowed_roles
             elif view.action == 'create':
                 return request.data['status'] == 'PENDING' and request.user.role in ['ADMIN', 'MANAGER', 'WAREHOUSE_MANAGER']
             else:
-                return request.user.role in ['ADMIN', 'MANAGER', 'WAREHOUSE_MANAGER', 'STORE_MANAGER']
+                return True
         else:
             return False
         
     def has_object_permission(self, request, view, obj):
         if request.user.is_authenticated:
             if request.method in permissions.SAFE_METHODS:
-                return request.user.role in ['ADMIN', 'MANAGER', 'WAREHOUSE_MANAGER', 'STORE_MANAGER']
+                return request.user.role in self.allowed_roles
             if view.action in ['update', 'partial_update']:
-                if request.user.role in ['ADMIN', 'MANAGER']:
-                    return True
-                elif request.user.role == 'WAREHOUSE_MANAGER':
-                    return 'status' not in request.data and obj.status == 'PENDING'
-                elif request.user.role == 'STORE_MANAGER':
-                    return request.data['status'] == 'RECEIVED' and 'quantity' not in request.data
+                if request.data['status'] == 'RECEIVED' and 'quantity' not in request.data:
+                    return request.user.role in ['ADMIN', 'MANAGER', 'STORE_MANAGER'] and obj.status == 'PENDING'
+                elif not request.data['status'] and obj.status == 'PENDING':
+                    return request.user.role in ['ADMIN', 'WAREHOUSE_MANAGER', 'MANAGER']
+                elif request.data['status'] == 'CANCELLED':
+                    return request.user.role in self.allowed_roles and obj.status == 'RECEIVED'
                 else:
                     return False
             elif view.action == 'destroy':
-                return request.user.role in ['MANAGER', 'WAREHOUSE_MANAGER', 'ADMIN'] and obj.status == 'PENDING'
+                if obj.status == 'PENDING':
+                    return request.user.role in ['MANAGER', 'ADMIN'] or request.user.role == obj.created_by
+                elif obj.status == 'CANCELLED':
+                    return request.user.role in self.allowed_roles
+                else:
+                    return False
             else:
                 return False            
---
+++ sales/signals.py
@@ -0,0 +1,101 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import Order, OrderItem, Customer, StockTransfer
+from products.models import StoreStock, WarehouseStock
+
+@receiver(post_save, sender=OrderItem)
+@receiver(post_delete, sender=OrderItem)
+def create_update_delete_orderItems(sender, instance, created, **kwargs):
+    """
+    This signal function is used to automatically update the quantity and some other fields of the Order model 
+    whenever the OrderItem model is created or updated.
+    """
+    # we set order_id to the orderItem instance's order field
+    order_id = instance.order
+    # we filter the database for an order with that particular id and set it to order
+    order = Order.objects.get(id=order_id)
+
+    if order:
+        # we look for all order items with that particular order linked to it
+        all_order_items = order.orderItems.all()
+        total_amount = 0
+        # we loop through each order item found
+        for orderItem in all_order_items:
+            # we set a variable total_amount and add each total_price to it
+            total_amount = total_amount + orderItem['total_price']
+        # we set the total_amount calculated after the loop completes to the total_amount field of that particular order
+        order.total_amount = total_amount
+        # we then save the order 
+        order.save()
+
+@receiver(post_save, sender=Order)
+def update_order_paid_cancelled(sender, instance, created, **kwargs):
+    """
+    This signal function is used to check for when an updated order instance's status is paid.
+    If paid, StoreStock's quantity is decreased by the orderItem's quantity.
+    If cancelled, Store stock is reversed.
+    """
+    # We set status to the order instance's status field
+    status = instance.status
+    if status == 'PAID':
+        # if status is paid, then get all orderItems linked with the order instance being updated or created
+        all_order_items = instance.orderItems.all()
+        # loop through all the order items
+        for orderItem in all_order_items:
+            # we set the store_stock_id to each orderItem's product field
+            storeStock_id = orderItem['product']
+            # we use the id to retrieve the particular StoreStock
+            storeStock = StoreStock.objects.get(id=storeStock_id)
+            # we decrease each storestock's quantity by each orderItem's quantity
+            storeStock['quantity'] = storeStock['quantity'] - orderItem['quantity']
+            storeStock.save()
+    elif status == 'CANCELLED':
+        # if status is cancelled, we get all orderitems linked with the order instance
+        all_order_items = instance.orderItems.all()
+        # we loop through each order item
+        for orderItem in all_order_items:
+            # we set the storestock_id to each orderitem's product field
+            storeStock_id = orderItem['product']
+            # we retreive the exact storestock in question
+            storeStock = StoreStock.objects.get(id=storeStock_id)
+            # We increase the storestock's quantity by the orderitem's quantity
+            storeStock['quantity'] = storeStock['quantity'] + orderItem['quantity']
+            storeStock.save()
+
+@receiver(post_save, sender=StockTransfer)
+def update_stock_transfer(sender, instance, created, **kwargs):
+    """
+    This is a signal function that seeks to increase and decrease warehouse stocks and store stocks
+    when a stock transfer is updated.
+    """
+    status = instance.status
+    if status == 'RECEIVED':
+        # when the status is received, we get the warehousestock id from the instance's stock field
+        warehouseStock_id = instance.stock
+        # we use that id to retrieve the particular warehouse stock being transferred
+        warehouseStock = WarehouseStock.objects.get(id=warehouseStock_id)
+        # we decrease the warehouse stock's quantity by the instance's quantity
+        warehouseStock['quantity'] = warehouseStock['quantity'] - instance['quantity']
+        warehouseStock.save()
+        warehousestock_code = warehouseStock['stock_code']
+        store_id = instance.destination
+        # we use the warehouse stock code together with the store id - the destination to retrieve the exact store stock code
+        storestock = StoreStock.objects.get(stock_code=warehousestock_code, store=store_id)
+        # we increase the quantity of that particular stock code and store combination
+        storestock['quantity'] = storestock['quantity'] + instance['quantity']
+        storestock.save()
+    elif status == 'CANCELLED':
+        # when the status is received, we get the warehousestock id from the instance's stock field
+        warehouseStock_id = instance.stock
+        # we use that id to retrieve the particular warehouse stock being transferred
+        warehouseStock = WarehouseStock.objects.get(id=warehouseStock_id)
+        # we increase the warehouse stock's quantity by the instance's quantity
+        warehouseStock['quantity'] = warehouseStock['quantity'] + instance['quantity']
+        warehouseStock.save()
+        warehousestock_code = warehouseStock['stock_code']
+        store_id = instance.destination
+        # we use the warehouse stock code together with the store id - the destination to retrieve the exact store stock code
+        storestock = StoreStock.objects.get(stock_code=warehousestock_code, store=store_id)
+        # we decrease the quantity of that particular stock code and store combination
+        storestock['quantity'] = storestock['quantity'] - instance['quantity']
+        storestock.save()        
