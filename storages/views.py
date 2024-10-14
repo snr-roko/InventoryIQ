19a8acb0644c2f065728651f79e8cbdca634400b
mr-roko <rbbagyei@gmail.com>
2024-10-14 08:40:40 +0000

    Sorting added to storages

--- storages/views.py
+++ storages/views.py
@@ -2,11 +2,15 @@ from rest_framework import viewsets
 from .models import Warehouse, Store, Supplier
 from .serializers import WarehouseSerializer, StoreSerializer, SupplierSerializer
 from .permissions import WarehousePermission, StorePermission, SupplierPermission
+from rest_framework.filters import OrderingFilter
 
 class WarehouseViewSet(viewsets.ModelViewSet):
     queryset = Warehouse.objects.all()
     serializer_class = WarehouseSerializer
     permission_classes = (WarehousePermission,)
+    filter_backends = (OrderingFilter,)
+    ordering_fields = ["name", "location"]
+    ordering = ["name"]
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
@@ -15,6 +19,9 @@ class StoreViewSet(viewsets.ModelViewSet):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
     permission_classes = (StorePermission,)
+    filter_backends = (OrderingFilter,)
+    ordering_fields = ["name", "location"]
+    ordering = ["name"]
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
@@ -23,6 +30,9 @@ class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
     permission_classes = (SupplierPermission,)
+    filter_backends = (OrderingFilter,)
+    ordering_fields = ["name", "address"]
+    ordering = ["name"]
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
