1e2666c3244c9226f8e0da8720619d30db91fc8a
mr-roko <rbbagyei@gmail.com>
2024-10-18 21:01:46 +0000

    Environment variables configured

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-arq&0j)(z&g9e**785*-iv$c&*n#e2gu^&ql%*n-$a%x+6akk_'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
